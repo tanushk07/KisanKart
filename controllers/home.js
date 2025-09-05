674a2da169b3ab11b7d3aa5852098c3d77179d4b
tanushk07 <146575873+tanushk07@users.noreply.github.com>
2025-09-06 02:37:13 +0530

    sort fixes

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,11 @@
-const products = require('../models/products')
+const products = require("../models/products");
 
-let { sortCategory } = require('../utils/sortCategory')
+const { sortCategory } = require("../utils/sortCategory");
 
 module.exports.getHome = async (req, res, next) => {
   try {
-    let product = await products.find({})
-    let sortedProducts = sortCategory(product)
+    let product = await products.find({});
+    let sortedProducts = sortCategory(product);
 
     // if (req.user._id) {
     //   res.render('index', {
@@ -18,20 +18,19 @@ module.exports.getHome = async (req, res, next) => {
     //     products: sortedProducts,
     //   })
     // }
-    res.render('index', {
+    res.render("index", {
       products: sortedProducts,
-      user:req.user
-    })
-    } catch (err) {
-      next(err)
-    }
-
+      user: req.user,
+    });
+  } catch (err) {
+    next(err);
   }
+};
 
 module.exports.getLogin = async (req, res, next) => {
-    res.render('login')
-  }
+  res.render("login");
+};
 
-  module.exports.getSignup = async (req, res, next) => {
-    res.render('signup')
-  }
+module.exports.getSignup = async (req, res, next) => {
+  res.render("signup");
+};
